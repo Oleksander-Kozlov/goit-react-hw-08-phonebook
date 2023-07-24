3f419a451f62196f43d6ee6c8efed64845dab7f2
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-07-24 16:19:47 +0300

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,11 +7,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlise';
 import { filtersReducer } from './filtersSlise';
 import { authSliseReducer } from './auth/auth-slise';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+// import storage from 'redux-persist/lib/storage'
 
+const AuthPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist:['token'],
+}
+
+const PersistTokenReduser = persistReducer(AuthPersistConfig, authSliseReducer )
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filters: filtersReducer,
-    auth: authSliseReducer,
+    auth: PersistTokenReduser,
   },
 });
+
+export const persistor = persistStore(store);
